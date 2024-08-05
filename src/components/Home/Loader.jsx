3bb2db71845e6e85b8860e4bb812f53bd7e44077
Harman <2002hpsingh@gmail.com>
2024-08-05 16:31:29 +0530

    cart animation added

--- src/components/Home/Loader.jsx
+++ src/components/Home/Loader.jsx
@@ -1,12 +1,16 @@
 import React, { useContext} from 'react'
 import '../../styles/Loader.css'
 import {Context} from '../../index'
+import cart from "../../images/shoppingCart.png"
 
 const Loader = () => {
     const {isLoading,fact} = useContext(Context);
 
   return (
     <div id='Loading' style={{"display":isLoading?"flex":"none"}}>
+      <div id='animatedCart' style={{"display":fact==""?"none":"block"}}>
+        <img src={cart} alt="loading..." />
+      </div>
       <div id="ring"></div>
       <div id='fact'>{fact}</div>
     </div>
